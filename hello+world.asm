309e2049633b456ed4b5ed0b84dbe378bb8a30d9
Alex Graden <97418444+agraden@users.noreply.github.com>
2023-10-04 22:36:48 -0400

    hello world~

--- hello+world.asm
+++ hello+world.asm
@@ -1,15 +1,13 @@
-; Date: 10.4.2023
-
 global _start
 
 section .text:
 
 _start:
-    mov eax, 0x4    ; using write syscall
+    mov eax, 0x4
     mov ebx, 1
     mov ecx, message
     mov edx, message_length
-    int 0x80    ; invoke syscall
+    int 0x80
     mov eax, 0x1
     mov ebx, 0
     int 0x80
